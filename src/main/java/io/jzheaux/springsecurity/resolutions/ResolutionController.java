503acca2e154f166dde49090040d6710f4032ba3
FadiHaddad <fadi_haddad@live.se>
2021-11-03 15:30:27 +0100

    Completed module 6

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionController.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionController.java
@@ -18,14 +18,15 @@ import java.util.UUID;
 @RestController
 public class ResolutionController {
 	private final ResolutionRepository resolutions;
-	private final UserRepository users;
+	//private final UserRepository users;
+	private UserService users;
 
-	public ResolutionController(ResolutionRepository resolutions, UserRepository users) {
+	public ResolutionController(ResolutionRepository resolutions, UserService users) {
 		this.resolutions = resolutions;
 		this.users = users;
 	}
 
-	@CrossOrigin(allowCredentials = "true")
+	@CrossOrigin
 	@GetMapping("/resolutions")
 	@PreAuthorize("hasAuthority('resolution:read')")
 	@PostFilter("@post.filter(#root)")
@@ -35,8 +36,8 @@ public class ResolutionController {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		if(authentication.getAuthorities().contains(new SimpleGrantedAuthority("user:read"))) {
 			for (Resolution resolution : resolutions) {
-				String fullName = this.users.findByUsername(resolution.getOwner())
-						.map(User::getFullName).orElse("Anonymous");
+				String fullName = this.users.getFullName(resolution.getOwner())
+						.orElse("Anonymous");
 				resolution.setText(resolution.getText() + ", by " + fullName);
 			}
 		}
